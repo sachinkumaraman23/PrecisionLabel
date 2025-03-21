618d1905ba0917c28801f12cc485bc67a2fed744
sachikumaraman23 <sachinkumaraman23@gmail.com>
2025-03-21 23:53:49 +0530

    sure

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -10,6 +10,7 @@ function Header() {
   const [scrolled, setScrolled] = useState(false);
   const location = useLocation();
   const [activeKey, setActiveKey] = useState(location.pathname);
+  const [openDropdown, setOpenDropdown] = useState(null); // Track open dropdown
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,14 +23,19 @@ function Header() {
 
   useEffect(() => {
     setActiveKey(location.pathname);
+    setOpenDropdown(null); // Close dropdown when navigating
   }, [location]);
 
+  const handleDropdownToggle = (dropdown) => {
+    setOpenDropdown(openDropdown === dropdown ? null : dropdown);
+  };
+
+  const handleMenuItemClick = () => {
+    setOpenDropdown(null); // Close dropdown immediately when an item is clicked
+  };
+
   return (
-    <Navbar
-      expand="lg"
-      fixed="top"
-      className={`navbar-custom ${scrolled ? 'navbar-scrolled' : ''}`}
-    >
+    <Navbar expand="lg" fixed="top" className={`navbar-custom ${scrolled ? 'navbar-scrolled' : ''}`}>
       <Container>
         <Navbar.Brand href="/">
           <img src={Logo} alt="Logo" height="40" />
@@ -40,37 +46,52 @@ function Header() {
             <LinkContainer to="/">
               <Nav.Link eventKey="/">Home</Nav.Link>
             </LinkContainer>
-            <NavDropdown title="Services" id="services-dropdown">
+            
+            {/* Services Dropdown */}
+            <NavDropdown
+              title="Services"
+              id="services-dropdown"
+              show={openDropdown === "services"}
+              onClick={() => handleDropdownToggle("services")}
+            >
               <LinkContainer to="/text">
-                <NavDropdown.Item eventKey="/text">Text Annotation</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/text" onClick={handleMenuItemClick}>Text Annotation</NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/image">
-                <NavDropdown.Item eventKey="/image">Image Annotation</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/image" onClick={handleMenuItemClick}>Image Annotation</NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/audio">
-                <NavDropdown.Item eventKey="/audio">Audio Annotation</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/audio" onClick={handleMenuItemClick}>Audio Annotation</NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/video">
-                <NavDropdown.Item eventKey="/video">Video Annotation</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/video" onClick={handleMenuItemClick}>Video Annotation</NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
-            <NavDropdown title="Use Cases" id="use-cases-dropdown">
+
+            {/* Use Cases Dropdown */}
+            <NavDropdown
+              title="Use Cases"
+              id="use-cases-dropdown"
+              show={openDropdown === "useCases"}
+              onClick={() => handleDropdownToggle("useCases")}
+            >
               <LinkContainer to="/drone_satellites">
-                <NavDropdown.Item eventKey="/drone_satellites">Drone Satellites</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/drone_satellites" onClick={handleMenuItemClick}>Drone Satellites</NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/facial_recognitio">
-                <NavDropdown.Item eventKey="/facial_recognitio">Facial Recognition</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/facial_recognitio" onClick={handleMenuItemClick}>Facial Recognition</NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/Self_driving_cars">
-                <NavDropdown.Item eventKey="/Self_driving_cars">Self Driving Cars</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/Self_driving_cars" onClick={handleMenuItemClick}>Self Driving Cars</NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/Sports_annotation">
-                <NavDropdown.Item eventKey="/Sports_annotation">Sports Annotation</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/Sports_annotation" onClick={handleMenuItemClick}>Sports Annotation</NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/Video_motion_tracking">
-                <NavDropdown.Item eventKey="/Video_motion_tracking">Video Motion Tracking</NavDropdown.Item>
+                <NavDropdown.Item eventKey="/Video_motion_tracking" onClick={handleMenuItemClick}>Video Motion Tracking</NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
+
             <LinkContainer to="/contact">
               <Button className="contact-us-btn ms-2">Contact Us</Button>
             </LinkContainer>
